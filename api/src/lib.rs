d396c2140043e8c6a4908de9aa40d541d0278b00
Landen <brewerla02@gmail.com>
2026-08-31 08:02:27 -0500

    fixup! chore: add web and worker rs scripts to prepare for clap to be added for dynamic crates-io style dynamic worker/web server setup

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -5,18 +5,3 @@ pub mod routes;
 pub mod services;
 pub mod web;
 pub mod worker;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
