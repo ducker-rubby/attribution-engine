fd5e753a4f5af3735af8c7628d64d8e5dcc622ce
Landen <brewerla02@gmail.com>
2026-09-01 11:59:28 -0500

    feat(api): add click_ref to click event

--- api/src/models/click.rs
+++ api/src/models/click.rs
@@ -4,24 +4,27 @@ use crate::models::Event;
 pub struct ClickEvent<'a> {
     id: &'a str,
     metadata: Vec<(&'a str, &'a str)>,
+    click_ref: &'a str,
 }
 
 impl<'a> ClickEvent<'a> {
     pub fn build(id: &'a str) -> Self {
-        let metadata = vec![("link_id", id)];
-        Self { id, metadata }
+        let click_ref = ClickEvent::new_ref_id();
+        let metadata = vec![("link_id", id), ("click_ref", click_ref)];
+
+        Self {
+            id,
+            metadata,
+            click_ref,
+        }
+    }
+
+    fn new_ref_id() -> &'a str {
+        unimplemented!()
     }
 }
 
 impl Event for ClickEvent<'_> {
-    fn get_type(&self) -> &str {
-        unimplemented!()
-    }
-
-    fn get_id(&self) -> &str {
-        self.id
-    }
-
     fn get_metadata(&self) -> &[(&str, &str)] {
         &self.metadata
     }
@@ -35,7 +38,9 @@ mod tests {
     fn build_click_event() {
         let click_event = ClickEvent::build("abcd");
 
-        assert_eq!(click_event.get_id(), "abcd");
-        assert_eq!(click_event.get_metadata(), &[("link_id", "abcd")]);
+        assert_eq!(
+            click_event.get_metadata(),
+            &[("link_id", "abcd"), ("click_ref", "")]
+        );
     }
 }
--- api/src/models/event.rs
+++ api/src/models/event.rs
@@ -1,5 +1,3 @@
 pub trait Event {
-    fn get_id(&self) -> &str;
-    fn get_type(&self) -> &str;
     fn get_metadata(&self) -> &[(&str, &str)];
 }
