a7d22435c485ce449fa9101d25a29db74adac2e1
Landen <brewerla02@gmail.com>
2026-09-02 11:44:07 -0500

    feat(api): refactor conversion event - use new event style - modify enum to no longer hold data

--- api/src/models/conversion.rs
+++ api/src/models/conversion.rs
@@ -1,43 +1,45 @@
 use crate::models::Event;
-use std::error::Error;
+use std::{error::Error, fmt};
 
-pub enum ConversionType<'a> {
-    SignUp(&'a str),
-    Sale(&'a str),
+pub enum ConversionType {
+    SignUp,
+    Sale,
 }
 
-impl<'a> TryFrom<&str> for ConversionType<'a> {
+impl<'a> TryFrom<&str> for ConversionType {
     type Error = Box<dyn Error>;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         match value {
-            "sign-up" => Ok(Self::SignUp("sign-up")),
-            "sale" => Ok(Self::Sale("sale")),
-            _ => Ok(Self::Sale("sale")),
+            "sign-up" => Ok(Self::SignUp),
+            "sale" => Ok(Self::Sale),
+            _ => Err(Box::from("No conversion type found")),
+        }
+    }
+}
+
+impl fmt::Display for ConversionType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ConversionType::SignUp => write!(f, "sign-up"),
+            ConversionType::Sale => write!(f, "sale"),
         }
     }
 }
 
 pub struct ConversionEvent<'a> {
-    conversion_type: ConversionType<'a>,
+    conversion_type: ConversionType,
+    conversion_type_str: String,
     click_ref: &'a str,
-    metadata: Vec<(&'a str, &'a str)>,
 }
 
 impl<'a> ConversionEvent<'a> {
     pub fn build(click_ref: &'a str, conversion_type: &'a str) -> Result<Self, Box<dyn Error>> {
-        let this_type: ConversionType<'a> = conversion_type.try_into()?;
-        let metadata = vec![("click_ref", click_ref), ("conversion_type", "")];
-
-        Ok(Self {
-            conversion_type: this_type,
-            click_ref,
-            metadata,
-        })
+        unimplemented!()
     }
 }
 
 impl<'a> Event for ConversionEvent<'a> {
-    fn get_metadata(&self) -> &[(&str, &str)] {
-        unimplemented!()
+    fn get_metadata(&self) -> Vec<(&str, &str)> {
+        vec![("conversion_type", self.conversion_type_str.as_str())]
     }
 }
